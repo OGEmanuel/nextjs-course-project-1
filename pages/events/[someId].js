352ebb68a123860c81a7af7dda3aba8e12b2f4f4
OGEmanuel <1blacksanta416@gmail.com>
2023-03-06 23:53:56 +0100

    final commit

--- pages/events/[someId].js
+++ pages/events/[someId].js
@@ -1,6 +1,7 @@
 import EventContent from '@/components/event-detail/event-content';
 import EventLogistics from '@/components/event-detail/event-logistics';
 import EventSummary from '@/components/event-detail/event-summary';
+import ErrorAlert from '@/components/UI/error-alert';
 import { getEventById } from '@/dummy-data';
 import { useRouter } from 'next/router';
 
@@ -11,7 +12,11 @@ const EventsDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No Event found!</p>
+      </ErrorAlert>
+    );
   }
 
   return (
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,22 @@
+import EventList from '@/components/events/event-list';
+import EventsSearch from '@/components/events/events-search';
+import { getAllEvents } from '@/dummy-data';
+import { useRouter } from 'next/router';
 const EventsPage = () => {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
+
   return (
-    <div>
-      <h1>Events Page</h1>
-    </div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
   );
 };
 
